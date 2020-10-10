bf5eeccaaacc93b7766dcfdb01435ee8c34c2f1b
stalvatero <64108632+stalvatero@users.noreply.github.com>
2020-10-10 17:47:51 +0200

    pyxis: initial commit firmware from Pyxis_Global_V12.0.3.0.QFCMIXM

---
+++ pyxis/firmware.mk
@@ -0,0 +1,20 @@
+# Firmware
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi-firmware/pyxis/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi-firmware/pyxis/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi-firmware/pyxis/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi-firmware/pyxis/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi-firmware/pyxis/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi-firmware/pyxis/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi-firmware/pyxis/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi-firmware/pyxis/dtbo.img:install/firmware-update/dtbo.img \
+    vendor/xiaomi-firmware/pyxis/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi-firmware/pyxis/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+    vendor/xiaomi-firmware/pyxis/logfs_ufs_8mb.bin:install/firmware-update/logfs_ufs_8mb.bin \
+    vendor/xiaomi-firmware/pyxis/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi-firmware/pyxis/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi-firmware/pyxis/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi-firmware/pyxis/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi-firmware/pyxis/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi-firmware/pyxis/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi-firmware/pyxis/xbl_config.elf:install/firmware-update/xbl_config.elf
